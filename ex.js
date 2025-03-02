df277e320bac63b24746458b82e37e393eb4611d
giovanni-arduini <gionnibigudd@gmail.com>
2025-03-02 17:43:33 +0100

    REFACTORY code correction

--- ex.js
+++ ex.js
@@ -1,52 +1,16 @@
-// In questo esercizio, utilizzerai async/await per creare la funzione getChefBirthday(id). 
-// Questa funzione accetta un id di una ricetta e deve:
-// Recuperare la ricetta da https://dummyjson.com/recipes/{id}
-// Estrarre la proprietà userId dalla ricetta
-// Usare userId per ottenere le informazioni dello chef da https://dummyjson.com/users/{userId}
-// Restituire la data di nascita dello chef
-
-async function fetchJson(url){
-    const response = await fetch(url);
-    const obj = await response.json()
-    return obj
+async function getChefBirthday(id) {
+    const recipeResponse = await fetch(`https://dummyjson.com/recipes/${id}`);
+    const recipe = await recipeResponse.json(); 
+    const chefResponse = await fetch(`https://dummyjson.com/users/${recipe.userId}`)
+    const chef = await chefResponse.json()
+    return chef.birthday
 }
 
-const getChefBirthday = async (id)=>{
-
-
-    let userId;
-    try{
-         userId = await fetchJson(`https://dummyjson.com/recipes/${id}`)
-        .then(data=>data.userId)
-    }catch(error){
-       throw new Error("Non è possibile recuperare l'id dell'utente")
-    }
-    if(userId.message){throw new Error(response.message)}
-
-    let chef 
-    try{await fetchJson(`https://dummyjson.com/users/${userId}`)
-    .then(response => chef = response.birthDate)
+( async()=>{
+    try {
+        const birthday = await getChefBirthday(1)
+        console.log("Data di nascita dello chef:", birthday)
     }catch{
-        throw new Error("Non è stato trovato un utente con questo id")
+        console.error(error);
     }
-    if(response.message){throw new Error(response.message)}
-
-    return chef
-}
-
-// IIFE
-(async()=>{
-    try{
-        const birthday = await getChefBirthday(2)
-        console.log("La data di nascita dello chef è:", birthday)
-    }catch(error){
-       console.log(error)
-    }
-})()
-
-
-// PRIMA VERSIONE 
-// getChefBirthday(2)
-// .then(birthday=>(console.log("La data di nascita dello chef è:", birthday)))
-// .catch(error=>console.error(error))
-// .finally(console.log("Fine del codice"))
+} )

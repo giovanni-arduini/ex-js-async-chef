8e6ee15fec60a989626744e359c04f4ae905c6e8
giovanni-arduini <gionnibigudd@gmail.com>
2025-03-02 17:48:29 +0100

    FIX

--- ex.js
+++ ex.js
@@ -1,8 +1,29 @@
 async function getChefBirthday(id) {
-    const recipeResponse = await fetch(`https://dummyjson.com/recipes/${id}`);
-    const recipe = await recipeResponse.json(); 
-    const chefResponse = await fetch(`https://dummyjson.com/users/${recipe.userId}`)
-    const chef = await chefResponse.json()
+
+    let recipe
+    try {   const recipeResponse = await fetch(`https://dummyjson.com/recipes/${id}`);
+            recipe = await recipeResponse.json(); 
+        }catch(error){
+            console.error(error);
+            throw new Error(`Non recupero la ricetta con id ${id}`)
+        }
+
+    if (recipe.message){
+        throw new Error(`Ricetta con id ${id} non trovata`)
+    }
+    
+    let chef
+    try{
+        const chefResponse = await fetch(`https://dummyjson.com/users/${recipe.userId}`)
+        chef = await chefResponse.json()
+    }catch(error){
+        console.error(error);
+        throw new Error(`Non recupero lo chef con id ${id}`)
+    }
+    if (recipe.message){
+        throw new Error(`Chef con id ${id} non trovata`)
+    }
+
     return chef.birthday
 }
 
